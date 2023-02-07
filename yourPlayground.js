15dc3b404c66f2d5948d3b29e062b1f82090e120
mirazedewan <miraze.dewan@ennova-research.com>
2023-02-07 17:48:30 +0100

    update your playground

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,13 +1,42 @@
 
-const introducer = (name, role) => {
+// Objects
 
+const myIntro = (name, role, company) => {
     const person = {
         name: name,
-        role: role
+        role: role,
+        company: company,
+        assets: 130000,
+        liabilites: 50000,
+        netWorth: function () {
+            return this.assets - this.liabilites;
+        }
     }
-
-    const intro = `Hi, my name is ${person.name} and I am ${person.role} at Ennova Research SRL`;
+    const intro = `Hi, my name is ${person.name} and I am ${person.role} at ${person.company}. My net worth is $${person.netWorth()}`;
     return intro
 }
+// console.log(myIntro("Miraze", "Head of Design", "Ennova Research SRL"))
 
-console.log(introducer("Miraze", "Head of Design"))
+const frutta = ['🍏','🍐','🍉','🍓','🍌','🍇'];
+
+// for (let i = 0; i < frutta.length; i++ ) {
+//     console.log(i, frutta[i]);
+// }
+
+for (const frut of frutta) {
+    // console.log(frut);
+}
+
+
+
+const numeri = [1, 2, 3, 4, 5, 6]
+
+let numerix2 = [];
+
+for (const numero of numeri) {
+    numerix2.push(numero ** 2);
+}
+
+// console.log(numerix2);
+
+
